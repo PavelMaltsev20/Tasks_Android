1b6e395a992f6f53a18e0d27b7757f2bcd77fa2b
Pavel <maltsevpavel95@gmail.com>
2021-10-13 14:09:49 +0300

    Create LoginFragment.kt background title and image, add fonts

--- app/src/main/java/com/pavelmaltsev/tasks/auth/AuthFragment.kt
+++ app/src/main/java/com/pavelmaltsev/tasks/auth/AuthFragment.kt
@@ -1,4 +1,79 @@
 package com.pavelmaltsev.tasks.auth
 
-class AuthFragment {
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.pavelmaltsev.tasks.R
+
+class AuthFragment :Fragment(){
+
+    private val callbackManager = CallbackManager.Factory.create()
+    private val TAG ="tester"
+
+    fun authWithGoogle(){
+        //        LoginManager.getInstance().logOut();
+//
+//        sdkInitialize(FacebookSdk.getApplicationContext())
+//        binding.loginButton.setReadPermissions("email", "public_profile")
+//
+//        binding.loginButton.registerCallback(
+//            callbackManager,
+//            object : FacebookCallback<LoginResult> {
+//                override fun onCancel() {
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        getString(R.string.action_canceled_please_check_facebook_login_and_password),
+//                        Toast.LENGTH_LONG
+//                    ).show()
+//                }
+//
+//                override fun onError(error: FacebookException) {
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        getString(R.string.action_failed_please_check_internet_connection),
+//                        Toast.LENGTH_LONG
+//                    ).show()
+//
+//                }
+//
+//                override fun onSuccess(result: LoginResult) {
+//                    firebaseAuthWithFacebook(result.accessToken)
+//                }
+//            });
+    }
+
+
+    /**
+     *  super.onActivityResult - deprecated, but facebook still use this method
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == AppCompatActivity.RESULT_OK) {
+            callbackManager.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    fun firebaseAuthWithFacebook(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val facebookUser = auth.currentUser!!
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.welcome_to_tasks_app),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Log.e(TAG, "signInWithCredential error: ${task.exception}")
+                }
+            }
+    }
 }
--- app/src/main/java/com/pavelmaltsev/tasks/auth/LoginFragment.kt
+++ app/src/main/java/com/pavelmaltsev/tasks/auth/LoginFragment.kt
@@ -6,17 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pavelmaltsev.tasks.R
+import com.pavelmaltsev.tasks.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding
+    }
 }
