d652874ae5b40bd3f339588b760055c308b636fc
Pavel <maltsevpavel95@gmail.com>
2021-10-14 03:37:18 +0300

    Create database

---
+++ app/src/main/java/com/pavelmaltsev/tasks/data/room/MainDatabase.kt
@@ -0,0 +1,34 @@
+package com.pavelmaltsev.tasks.data.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.pavelmaltsev.tasks.module.Task
+
+@Database(
+    entities = [
+        Task::class,
+    ], version = 1, exportSchema = false
+)
+abstract class MainDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MainDatabase? = null
+
+        fun getDatabase(context: Context): MainDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context): MainDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                MainDatabase::class.java,
+                "tasks_data"
+            ).build()
+        }
+    }
+}
---
+++ app/src/main/java/com/pavelmaltsev/tasks/data/room/TaskRepository.kt
@@ -0,0 +1,4 @@
+package com.pavelmaltsev.tasks.data.room
+
+class TaskRepository {
+}
