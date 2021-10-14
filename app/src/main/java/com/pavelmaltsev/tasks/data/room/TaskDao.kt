d06b706a2de0804b156ad0af68600c3fe62a09fc
Pavel <maltsevpavel95@gmail.com>
2021-10-14 03:39:17 +0300

    Create task module/ table

--- app/src/main/java/com/pavelmaltsev/tasks/data/room/TaskDao.kt
+++ app/src/main/java/com/pavelmaltsev/tasks/data/room/TaskDao.kt
@@ -1,4 +1,22 @@
 package com.pavelmaltsev.tasks.data.room
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
 class TaskDao {
+
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: User)
+
+    @Update
+    fun update(user: User)
+
+    @Delete
+    fun deleteCurrent(user: User)
+
+
+    @Query("SELECT * FROM USER_TABLE WHERE id != :userId ORDER BY lastMsg, userName DESC")
+    fun getList(userId: String): LiveData<List<User>>
+
 }
--- app/src/main/java/com/pavelmaltsev/tasks/module/Task.kt
+++ app/src/main/java/com/pavelmaltsev/tasks/module/Task.kt
@@ -1,9 +1,12 @@
 package com.pavelmaltsev.tasks.module
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
-
+@Entity(tableName = "TASK_TABLE")
 data class Task(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val date: Date = Date(),
     val title: String = "",
